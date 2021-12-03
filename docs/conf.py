8a20c2099e9eea5235b1dc819ef8c633b21ab662
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2021-12-02 19:47:13 -0600

    Add source code to RTD

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,6 @@ def skip(app, what, name, obj, would_skip, options):
         return False
     return would_skip
 
-
 def setup(app):
     """Setup sphinx docs"""
     app.connect("autodoc-skip-member", skip)
@@ -43,7 +42,12 @@ todo_include_todos = True
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.todo']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 # templates_path = ['_templates']
