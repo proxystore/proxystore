eebd2c05c60dcf6872246e5b524a8535ade34e6a
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2021-12-17 15:45:44 -0600

    Object/class fullname helper

---
+++ proxystore/test/test_utils.py
@@ -0,0 +1,15 @@
+"""Utils Unit Tests"""
+from proxystore import utils
+from proxystore.factory import SimpleFactory
+
+
+def test_create_key() -> None:
+    """Test create_key()"""
+    assert isinstance(utils.create_key(42), str)
+
+
+def test_fullname() -> None:
+    """Test fullname()"""
+    assert utils.fullname(SimpleFactory) == 'proxystore.factory.SimpleFactory'
+    assert utils.fullname(SimpleFactory('string')) == 'proxystore.factory.SimpleFactory'
+    assert utils.fullname('string') == 'str'
