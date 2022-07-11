4ffe93ca5d8f12a9407b8cd2452394daafb179bf
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2022-07-11 10:42:48 -0500

    Move quart routes to blueprint

--- proxystore/endpoint/serve.py
+++ proxystore/endpoint/serve.py
@@ -19,6 +19,8 @@ logger = logging.getLogger(__name__)
 quart.logging.default_handler = logging.NullHandler()  # type: ignore
 quart.logging.serving_handler = logging.NullHandler()  # type: ignore
 
+routes_blueprint = quart.Blueprint('routes', __name__)
+
 
 def create_app(
     endpoint: Endpoint,
@@ -45,128 +47,9 @@ def create_app(
     app_logger.handlers = logger.handlers
     serving_logger.handlers = logger.handlers
 
-    @app.before_serving
-    async def startup() -> None:
-        await endpoint.async_init()
+    app.config['endpoint'] = endpoint
 
-    @app.after_serving
-    async def shutdown() -> None:
-        await endpoint.close()
-
-    @app.route('/')
-    async def home() -> tuple[str, int]:
-        return ('', 200)
-
-    @app.route('/endpoint', methods=['GET'])
-    async def endpoint_() -> Response:
-        return Response(
-            json.dumps({'uuid': str(endpoint.uuid)}),
-            200,
-            content_type='application/json',
-        )
-
-    @app.route('/evict', methods=['POST'])
-    async def evict() -> Response:
-        key = request.args.get('key', None)
-        if key is None:
-            return Response('request missing key', 400)
-
-        endpoint_uuid: str | uuid.UUID | None = request.args.get(
-            'endpoint',
-            None,
-        )
-        if isinstance(endpoint_uuid, str):
-            try:
-                endpoint_uuid = uuid.UUID(endpoint_uuid, version=4)
-            except ValueError:
-                return Response(f'{endpoint_uuid} is not a valid UUID4', 400)
-
-        try:
-            await endpoint.evict(key=key, endpoint=endpoint_uuid)
-            return Response('', 200)
-        except PeerRequestError as e:
-            return Response(str(e), 400)
-
-    @app.route('/exists', methods=['GET'])
-    async def exists() -> Response:
-        key = request.args.get('key', None)
-        if key is None:
-            return Response('request missing key', 400)
-
-        endpoint_uuid: str | uuid.UUID | None = request.args.get(
-            'endpoint',
-            None,
-        )
-        if isinstance(endpoint_uuid, str):
-            try:
-                endpoint_uuid = uuid.UUID(endpoint_uuid, version=4)
-            except ValueError:
-                return Response(f'{endpoint_uuid} is not a valid UUID4', 400)
-
-        try:
-            exists = await endpoint.exists(key=key, endpoint=endpoint_uuid)
-            return Response(
-                json.dumps({'exists': exists}),
-                200,
-                content_type='application/json',
-            )
-        except PeerRequestError as e:
-            return Response(str(e), 400)
-
-    @app.route('/get', methods=['GET'])
-    async def get() -> Response:
-        key = request.args.get('key', None)
-        if key is None:
-            return Response('request missing key', 400)
-
-        endpoint_uuid: str | uuid.UUID | None = request.args.get(
-            'endpoint',
-            None,
-        )
-        if isinstance(endpoint_uuid, str):
-            try:
-                endpoint_uuid = uuid.UUID(endpoint_uuid, version=4)
-            except ValueError:
-                return Response(f'{endpoint_uuid} is not a valid UUID4', 400)
-
-        try:
-            data = await endpoint.get(key=key, endpoint=endpoint_uuid)
-        except PeerRequestError as e:
-            return Response(str(e), 400)
-
-        if data is not None:
-            return Response(
-                response=data,
-                content_type='application/octet-stream',
-            )
-        else:
-            return Response('', 400)
-
-    @app.route('/set', methods=['POST'])
-    async def set() -> Response:
-        key = request.args.get('key', None)
-        if key is None:
-            return Response('request missing key', 400)
-
-        endpoint_uuid: str | uuid.UUID | None = request.args.get(
-            'endpoint',
-            None,
-        )
-        if isinstance(endpoint_uuid, str):
-            try:
-                endpoint_uuid = uuid.UUID(endpoint_uuid, version=4)
-            except ValueError:
-                return Response(f'{endpoint_uuid} is not a valid UUID4', 400)
-
-        try:
-            await endpoint.set(
-                key=key,
-                data=await request.get_data(),
-                endpoint=endpoint_uuid,
-            )
-            return Response('', 200)
-        except PeerRequestError as e:
-            return Response(str(e), 400)
+    app.register_blueprint(routes_blueprint, url_prefix='')
 
     logger.info(
         'quart routes registered to endpoint '
@@ -228,3 +111,142 @@ def serve(
         f'serving endpoint {endpoint.uuid} ({endpoint.name}) on {host}:{port}',
     )
     app.run(host=host, port=port)
+
+
+@routes_blueprint.before_app_serving
+async def _startup() -> None:
+    endpoint = quart.current_app.config['endpoint']
+    await endpoint.async_init()
+
+
+@routes_blueprint.after_app_serving
+async def _shutdown() -> None:
+    endpoint = quart.current_app.config['endpoint']
+    await endpoint.close()
+
+
+@routes_blueprint.route('/')
+async def _home() -> tuple[str, int]:
+    return ('', 200)
+
+
+@routes_blueprint.route('/endpoint', methods=['GET'])
+async def _endpoint_() -> Response:
+    endpoint = quart.current_app.config['endpoint']
+    return Response(
+        json.dumps({'uuid': str(endpoint.uuid)}),
+        200,
+        content_type='application/json',
+    )
+
+
+@routes_blueprint.route('/evict', methods=['POST'])
+async def _evict() -> Response:
+    key = request.args.get('key', None)
+    if key is None:
+        return Response('request missing key', 400)
+
+    endpoint_uuid: str | uuid.UUID | None = request.args.get(
+        'endpoint',
+        None,
+    )
+    endpoint = quart.current_app.config['endpoint']
+    if isinstance(endpoint_uuid, str):
+        try:
+            endpoint_uuid = uuid.UUID(endpoint_uuid, version=4)
+        except ValueError:
+            return Response(f'{endpoint_uuid} is not a valid UUID4', 400)
+
+    try:
+        await endpoint.evict(key=key, endpoint=endpoint_uuid)
+        return Response('', 200)
+    except PeerRequestError as e:
+        return Response(str(e), 400)
+
+
+@routes_blueprint.route('/exists', methods=['GET'])
+async def _exists() -> Response:
+    key = request.args.get('key', None)
+    if key is None:
+        return Response('request missing key', 400)
+
+    endpoint_uuid: str | uuid.UUID | None = request.args.get(
+        'endpoint',
+        None,
+    )
+    endpoint = quart.current_app.config['endpoint']
+    if isinstance(endpoint_uuid, str):
+        try:
+            endpoint_uuid = uuid.UUID(endpoint_uuid, version=4)
+        except ValueError:
+            return Response(f'{endpoint_uuid} is not a valid UUID4', 400)
+
+    try:
+        exists = await endpoint.exists(key=key, endpoint=endpoint_uuid)
+        return Response(
+            json.dumps({'exists': exists}),
+            200,
+            content_type='application/json',
+        )
+    except PeerRequestError as e:
+        return Response(str(e), 400)
+
+
+@routes_blueprint.route('/get', methods=['GET'])
+async def _get() -> Response:
+    key = request.args.get('key', None)
+    if key is None:
+        return Response('request missing key', 400)
+
+    endpoint_uuid: str | uuid.UUID | None = request.args.get(
+        'endpoint',
+        None,
+    )
+    endpoint = quart.current_app.config['endpoint']
+    if isinstance(endpoint_uuid, str):
+        try:
+            endpoint_uuid = uuid.UUID(endpoint_uuid, version=4)
+        except ValueError:
+            return Response(f'{endpoint_uuid} is not a valid UUID4', 400)
+
+    try:
+        data = await endpoint.get(key=key, endpoint=endpoint_uuid)
+    except PeerRequestError as e:
+        return Response(str(e), 400)
+
+    if data is not None:
+        return Response(
+            response=data,
+            content_type='application/octet-stream',
+        )
+    else:
+        return Response('', 400)
+
+
+@routes_blueprint.route('/set', methods=['POST'])
+async def _set() -> Response:
+    key = request.args.get('key', None)
+    if key is None:
+        return Response('request missing key', 400)
+
+    endpoint_uuid: str | uuid.UUID | None = request.args.get(
+        'endpoint',
+        None,
+    )
+    endpoint = quart.current_app.config['endpoint']
+    if isinstance(endpoint_uuid, str):
+        try:
+            endpoint_uuid = uuid.UUID(endpoint_uuid, version=4)
+        except ValueError:
+            return Response(f'{endpoint_uuid} is not a valid UUID4', 400)
+
+    try:
+        await endpoint.set(
+            key=key,
+            data=await request.get_data(),
+            endpoint=endpoint_uuid,
+        )
+    except PeerRequestError as e:
+        return Response(str(e), 400)
+    else:
+        return Response('', 200)
