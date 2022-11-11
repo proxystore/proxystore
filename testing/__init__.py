d0a0b787ebf16cfdaa1700008882c583f591a494
valhayot <valeriehayot@gmail.com>
2022-11-11 11:04:18 -0600

    moved pymargo and ucx mocker imports and added try except around them

--- testing/__init__.py
+++ testing/__init__.py
@@ -3,12 +3,5 @@ from __future__ import annotations
 
 import sys
 
-from testing.mocker_modules import pymargo_mocker
-from testing.mocker_modules import ucx_mocker
 
 # pymargo and ucx mocker imports
-
-sys.modules['pymargo'] = pymargo_mocker
-sys.modules['pymargo.bulk'] = pymargo_mocker
-sys.modules['pymargo.core'] = pymargo_mocker
-sys.modules['ucp'] = ucx_mocker
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,25 @@
 """Public fixtures for unit tests."""
 from __future__ import annotations
 
+import sys
+
+try:
+    import pymargo
+except ImportError:
+    from testing.mocker_modules import pymargo_mocker
+
+    sys.modules['pymargo'] = pymargo_mocker
+    sys.modules['pymargo.bulk'] = pymargo_mocker
+    sys.modules['pymargo.core'] = pymargo_mocker
+
+try:
+    import ucp
+except ImportError:
+    from testing.mocker_modules import ucx_mocker
+
+    sys.modules['ucp'] = ucx_mocker
+
+
 from testing.signaling_server import signaling_server
 from testing.store_utils import endpoint_store
 from testing.store_utils import file_store
