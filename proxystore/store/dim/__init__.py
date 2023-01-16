30cc0ec0bcb10a03c49bb844ff1d3414bf5fd5d6
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2023-01-16 16:18:42 -0600

    Add experimental warning to proxystore.store.dim

--- proxystore/store/dim/__init__.py
+++ proxystore/store/dim/__init__.py
@@ -1,2 +1,20 @@
 """Distributed in-memory stores."""
 from __future__ import annotations
+
+import warnings
+
+
+class ExperimentalWarning(Warning):
+    """Warning for experimental features."""
+
+    pass
+
+
+warnings.warn(
+    'The proxystore.store.dim module contains experimental code that may have '
+    'known issues or is subject to change in future releases. See the issue '
+    'tracker for the latest updates: '
+    'https://github.com/proxystore/proxystore/issues.',
+    ExperimentalWarning,
+    stacklevel=2,
+)
