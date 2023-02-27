8d76893a0225830eea3527b5464aa051e5ec4bae
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2023-02-27 09:15:47 -0600

    Fix mypy attr-defined error

--- proxystore/store/base.py
+++ proxystore/store/base.py
@@ -34,6 +34,7 @@ from proxystore.store.stats import FunctionEventStats
 from proxystore.store.stats import STORE_METHOD_KEY_IS_RESULT
 from proxystore.store.stats import TimeStats
 from proxystore.store.utils import get_key
+from proxystore.utils import fullname
 
 _default_pool = ThreadPoolExecutor()
 logger = logging.getLogger(__name__)
@@ -277,7 +278,7 @@ class Store(Generic[KeyT], metaclass=ABCMeta):
         self.close()
 
     def __repr__(self) -> str:
-        s = f'{ps.utils.fullname(self.__class__)}('
+        s = f'{fullname(self.__class__)}('
         attributes = [
             f'{key}={value}'
             for key, value in self.__dict__.items()
