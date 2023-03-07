17a225d5c174920b73d0a1c0bcfb438cb7616df9
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2023-03-07 17:48:53 -0600

    Add timer module with Timer class (#216)

---
+++ tests/timer_test.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+import time
+
+import pytest
+
+from proxystore.timer import Timer
+
+
+def test_timer() -> None:
+    timer = Timer()
+    timer.start()
+    time.sleep(0.001)
+    timer.stop()
+
+    assert timer.elapsed_ns > 1000
+    assert timer.elapsed_ms > 1
+    assert timer.elapsed_s > 0.001
+
+
+def test_timer_context_manger() -> None:
+    with Timer() as timer:
+        time.sleep(0.001)
+
+    assert timer.elapsed_ns > 1000
+    assert timer.elapsed_ms > 1
+    assert timer.elapsed_s > 0.001
+
+
+def test_timer_still_running() -> None:
+    with Timer() as timer:
+        with pytest.raises(RuntimeError, match='Timer is still running!'):
+            timer.elapsed_ns
+
+        with pytest.raises(RuntimeError, match='Timer is still running!'):
+            timer.elapsed_ms
+
+        with pytest.raises(RuntimeError, match='Timer is still running!'):
+            timer.elapsed_s
