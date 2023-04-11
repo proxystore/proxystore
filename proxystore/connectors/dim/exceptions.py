61187c77d3e6aaad6262591756cfe2d82aa2af8c
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2023-04-11 10:17:16 -0500

    Add custom DIM exceptions

---
+++ proxystore/connectors/dim/exceptions.py
@@ -0,0 +1,8 @@
+"""Exception types."""
+from __future__ import annotations
+
+
+class ServerTimeoutError(Exception):
+    """Error indicating client timed out while trying to connect to server."""
+
+    pass
