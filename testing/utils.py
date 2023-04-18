110e50ba4dfcfc28dd54dbc605fbd851e2e2f2dd
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2023-04-17 22:08:29 -0500

    Fix socket already bound in async fixtures
    Some of the async fixtures that bind to ports (e.g., relay_server) would
    occasionally error with port already in use. This appears to happen on
    very fast computers (e.g., my Ryzen 7800x3d) but not on slower computers
    (e.g., GitHub actions runners). I suspect this is because the async
    tasks start the server so fast the OS hasn't fully cleaned up the
    resources associated with the bound ports. I also added some
    functionality just to make sure there isn't a race condition with
    multiple async fixtures getting the same port from open_port() before
    binding to it.

--- testing/utils.py
+++ testing/utils.py
@@ -3,15 +3,23 @@ from __future__ import annotations
 
 import socket
 
+_used_ports: set[int] = set()
+
 
 def open_port() -> int:
     """Return open port.
 
     Source: https://stackoverflow.com/questions/2838244
     """
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('', 0))
-    s.listen(1)
-    port = s.getsockname()[1]
-    s.close()
-    return port
+    global _used_ports
+
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind(('', 0))
+        s.listen(1)
+        port = s.getsockname()[1]
+        s.close()
+        if port not in _used_ports:  # pragma: no branch
+            _used_ports.add(port)
+            return port
