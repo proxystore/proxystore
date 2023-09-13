c984ae3b60d2fe1871bd657a77e1cd2a42a27185
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2023-09-13 18:07:17 -0500

    Add Globus token storage builder

---
+++ tests/globus/conftest.py
@@ -0,0 +1,17 @@
+"""Test fixtures for proxystore.globus."""
+from __future__ import annotations
+
+import pathlib
+from typing import Generator
+from unittest import mock
+
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def _patch_storage_dir(tmp_path: pathlib.Path) -> Generator[None, None, None]:
+    with mock.patch(
+        'proxystore.globus.storage.home_dir',
+        return_value=str(tmp_path / 'storage.db'),
+    ):
+        yield
