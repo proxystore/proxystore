72d61b788c8ec24af516d6d8aa2dadb841ce82cf
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2024-01-18 17:23:34 -0600

    Skip kafka tests on Python 3.12
    kafka-python 2.0.2 (the latest release at this time) is not compatible
    with Python 3.12. There's not really any option here but to skip the
    tests and omit the relevant files from coverage. It's a bit ugly, but it
    seems like there may be an updated version soon.
    
    https://github.com/dpkp/kafka-python/issues/2408#issuecomment-1848021107

--- tests/pubsub/kafka_test.py
+++ tests/pubsub/kafka_test.py
@@ -1,12 +1,30 @@
 from __future__ import annotations
 
+import sys
+
 import pytest
 
-from proxystore.pubsub.kafka import KafkaPublisher
-from proxystore.pubsub.kafka import KafkaSubscriber
-from testing.mocked.kafka import make_producer_consumer_pair
+try:
+    import kafka
+
+    from proxystore.pubsub.kafka import KafkaPublisher
+    from proxystore.pubsub.kafka import KafkaSubscriber
+    from testing.mocked.kafka import make_producer_consumer_pair
+
+    kafka_available = True
+except ImportError:  # pragma: no cover
+    kafka_available = False
+
+if kafka_available:  # pragma: no branch
+    kafka_version = tuple(kafka.__version__.split('.'))
+
+skip_py312 = not kafka_available or (
+    sys.version_info >= (3, 12) and kafka_version <= (2, 0, 2)
+)
+skip_py312_reason = 'kafka-python<=2.0.2 is not compatible with Python 3.12'
 
 
+@pytest.mark.skipif(skip_py312, reason=skip_py312_reason)
 def test_bad_publisher_default_topic() -> None:
     producer, _ = make_producer_consumer_pair()
 
@@ -14,6 +32,7 @@ def test_bad_publisher_default_topic() -> None:
         KafkaPublisher(producer, topics=['default'], default_topic='other')
 
 
+@pytest.mark.skipif(skip_py312, reason=skip_py312_reason)
 def test_publish_unknown_topic() -> None:
     topic = 'default'
     producer, _ = make_producer_consumer_pair(topic)
@@ -26,12 +45,14 @@ def test_publish_unknown_topic() -> None:
     publisher.close()
 
 
+@pytest.mark.skipif(skip_py312, reason=skip_py312_reason)
 def test_publisher_close_client_only() -> None:
     producer, _ = make_producer_consumer_pair()
     publisher = KafkaPublisher(producer)
     publisher.close(close_topics=False)
 
 
+@pytest.mark.skipif(skip_py312, reason=skip_py312_reason)
 def test_basic_publish_subscribe() -> None:
     producer, consumer = make_producer_consumer_pair('default')
     publisher = KafkaPublisher(producer)
