0140f78e1456917903f3fb1f7f2f28773e898e2a
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2024-04-22 10:38:08 -0500

    Update Globus Compute and Parsl examples

--- examples/parsl/run.sh
+++ examples/parsl/run.sh
@@ -4,18 +4,18 @@ PORT=59465
 redis-server --save "" --appendonly no --port $PORT &> /dev/null &
 REDIS=$!
 
-ARRAY_COUNT=100
-ARRAY_SIZE=100
+ARRAY_COUNT=10
+ARRAY_SIZE=1000
 
 echo "Started Redis on localhost:$PORT"
 
 echo "Running mapreduce_parsl.py without ProxyStore"
-python mapreduce_parsl.py -n $ARRAY_SIZE -s $ARRAY_SIZE
+python mapreduce_parsl.py -n $ARRAY_COUNT -s $ARRAY_SIZE
 
 echo "Running mapreduce_parsl.py with ProxyStore (Local)"
-python mapreduce_parsl.py -n $ARRAY_SIZE -s $ARRAY_SIZE --proxy
+python mapreduce_parsl.py -n $ARRAY_COUNT -s $ARRAY_SIZE --proxy
 
 echo "Running mapreduce_parsl.py with ProxyStore (Redis)"
-python mapreduce_parsl.py -n $ARRAY_SIZE -s $ARRAY_SIZE --proxy --redis-port $PORT
+python mapreduce_parsl.py -n $ARRAY_COUNT -s $ARRAY_SIZE --proxy --redis-port $PORT
 
 kill $REDIS
