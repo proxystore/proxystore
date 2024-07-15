42b57bfe425bbbe66f1b1a8816c772ca1454953e
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2024-07-15 11:59:23 -0500

    Separate/parametrize serialization tests

--- tests/serialize_test.py
+++ tests/serialize_test.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import io
+from typing import Any
 from unittest import mock
 
 import numpy
@@ -16,50 +17,52 @@ from proxystore.serialize import SerializationError
 from proxystore.serialize import serialize
 
 
-def test_serialization() -> None:
-    xb = b'test string'
-    b = serialize(xb)
-    assert deserialize(b) == xb
+@pytest.mark.parametrize(
+    'obj',
+    (
+        b'binary-string',
+        'normal-string',
+        [1, 2, 3],
+        numpy.array([[1, 2, 3], [4, 5, 6]]),
+        pandas.DataFrame([[1, 2, 3], [4, 5, 6]]),
+        polars.DataFrame([[1, 2, 3], [4, 5, 6]]),
+    ),
+)
+def test_serialize_objects(obj: Any) -> None:
+    serialized = serialize(obj)
+    deserialized = deserialize(serialized)
 
-    xs = 'test string'
-    b = serialize(xs)
-    assert deserialize(b) == xs
+    if isinstance(obj, numpy.ndarray):
+        assert numpy.array_equal(deserialized, obj)
+    elif isinstance(obj, (pandas.DataFrame, polars.DataFrame)):
+        assert deserialized.equals(obj)
+    else:
+        assert deserialized == obj
 
-    xa = [1, 2, 3]
-    b = serialize(xa)
-    assert deserialize(b) == xa
 
+def test_serialize_lambda() -> None:
     b = serialize(lambda: [1, 2, 3])
     f = deserialize(b)
     assert f() == [1, 2, 3]
 
-    xnp = numpy.array([1, 2, 3])
-    b = serialize(xnp)
-    assert numpy.array_equal(deserialize(b), xnp)
 
-    xpd = pandas.DataFrame([[1, 2, 3], [4, 5, 6]])
-    b = serialize(xpd)
-    assert deserialize(b).equals(xpd)
-
-    xpl = polars.DataFrame({'a': [1, 2, 3]})
-    b = serialize(xpl)
-    assert deserialize(b).equals(xpl)
-
+def test_deserialize_bad_input_type():
     with pytest.raises(ValueError):
-        # deserialize raises ValueError on non-bytes inputs
-        deserialize('xxx')  # type: ignore
+        deserialize('non-bytes-input')  # type: ignore
 
+
+def test_deserialize_bad_identifier():
     with pytest.raises(SerializationError):
         # No identifier
         deserialize(b'xxx')
 
     with pytest.raises(SerializationError):
-        # Fake identifier 'xxx'
+        # Fake identifier
         deserialize(b'99\nxxx')
 
 
-def test_cloudpickle_dumps_error() -> None:
-    with mock.patch('cloudpickle.dumps', side_effect=Exception()):
+def test_propagate_cloudpickle_dumps_error() -> None:
+    with mock.patch('cloudpickle.dump', side_effect=Exception()):
         with pytest.raises(
             SerializationError,
             match="Object of type <class 'function'> is not supported.",
@@ -67,9 +70,9 @@ def test_cloudpickle_dumps_error() -> None:
             serialize(lambda x: x + x)  # pragma: no cover
 
 
-def test_pickle_loads_error() -> None:
+def test_propagate_pickle_loads_error() -> None:
     v = serialize([1, 2, 3])
-    with mock.patch('pickle.loads', side_effect=Exception()):
+    with mock.patch('pickle.load', side_effect=Exception()):
         with pytest.raises(
             SerializationError,
             match="Failed to deserialize object with identifier b'06'.",
@@ -77,14 +80,20 @@ def test_pickle_loads_error() -> None:
             deserialize(v)
 
 
-def test_cloudpickle_loads_error() -> None:
+def test_propagate_cloudpickle_loads_error() -> None:
     v = serialize(lambda x: x + x)  # pragma: no cover
-    with mock.patch('cloudpickle.loads', side_effect=Exception()):
+    with mock.patch('cloudpickle.load', side_effect=Exception()):
         with pytest.raises(
             SerializationError,
             match="Failed to deserialize object with identifier b'07'.",
         ):
             deserialize(v)
+
+
+def test_numpy_supported() -> None:
+    serializer = _NumpySerializer()
+    assert serializer.supported(numpy.array([1, 2, 3]))
+    assert not serializer.supported([1, 2, 3])
 
 
 def test_numpy_serializer() -> None:
@@ -97,6 +106,12 @@ def test_numpy_serializer() -> None:
         assert numpy.array_equal(xn, deserialized)
 
 
+def test_pandas_supported() -> None:
+    serializer = _PandasSerializer()
+    assert serializer.supported(pandas.DataFrame({'a': [1, 2, 3]}))
+    assert not serializer.supported({'a': [1, 2, 3]})
+
+
 def test_pandas_serializer() -> None:
     serializer = _PandasSerializer()
     xp = pandas.DataFrame({'a': [1, 2, 3]})
@@ -107,6 +122,12 @@ def test_pandas_serializer() -> None:
         assert xp.equals(deserialized)
 
 
+def test_polars_supported() -> None:
+    serializer = _PolarsSerializer()
+    assert serializer.supported(polars.DataFrame({'a': [1, 2, 3]}))
+    assert not serializer.supported({'a': [1, 2, 3]})
+
+
 def test_polars_serializer() -> None:
     serializer = _PolarsSerializer()
     xpl = polars.DataFrame({'a': [1, 2, 3]})
@@ -115,21 +136,3 @@ def test_polars_serializer() -> None:
         buffer.seek(0)
         deserialized = serializer.deserialize(buffer)
         assert xpl.equals(deserialized)
-
-
-def test_numpy_supported() -> None:
-    serializer = _NumpySerializer()
-    assert serializer.supported(numpy.array([1, 2, 3]))
-    assert not serializer.supported([1, 2, 3])
-
-
-def test_pandas_supported() -> None:
-    serializer = _PandasSerializer()
-    assert serializer.supported(pandas.DataFrame({'a': [1, 2, 3]}))
-    assert not serializer.supported({'a': [1, 2, 3]})
-
-
-def test_polars_supported() -> None:
-    serializer = _PolarsSerializer()
-    assert serializer.supported(polars.DataFrame({'a': [1, 2, 3]}))
-    assert not serializer.supported({'a': [1, 2, 3]})
